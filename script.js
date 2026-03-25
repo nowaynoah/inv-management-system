f9b564e656f3009eeb4467ef360274739e270ae4
nowaynoah <amawani10@gmail.com>
2026-03-25 13:39:20 -0400

    added PerhishableProduct subclass

--- script.js
+++ script.js
@@ -14,7 +14,18 @@ class Product {
     }
 }
 
-// Testing Class/Constructor
+class PerishableProduct extends Product {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
+    }
+}
+
+// Testing Product Class/Constructor
 const apple = new Product("Apple", 2.5, 50);
 
 console.log(apple.toString());
