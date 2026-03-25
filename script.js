c87e49d80708cac73cad25ec432836e7493e049d
nowaynoah <amawani10@gmail.com>
2026-03-25 13:32:26 -0400

    Added Product Class with Constructor and methods and tested it

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("JavaScript connected successfully.");
+class Product {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+}
+
+// Testing Class/Constructor
+const apple = new Product("Apple", 2.5, 50);
+
+console.log(apple.toString());
+console.log(apple.getTotalValue());
